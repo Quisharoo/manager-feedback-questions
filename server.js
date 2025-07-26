9209b77833d27e3dd3d0334476bda4ce2983cfe3
Quisharoo <c.quish1@nuigalway.ie>
2025-07-26 21:40:54 +0100

    Add Jest tests for server and question UI

--- server.js
+++ server.js
@@ -10,6 +10,10 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
-});
+if (require.main === module) {
+  app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+  });
+}
+
+module.exports = app;
