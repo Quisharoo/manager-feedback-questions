073e03b4ff16a7963320a0d03b18defb7ca6f30b
Quisharoo <colmquish@gmail.com>
2025-09-14 08:51:45 +0100

    refactor(server): move session store to server/sessionStore.js; docs: add project structure to README

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const path = require('path');
-const { createSession, getSession, saveSession } = require('./sessionStore');
+const { createSession, getSession, saveSession } = require('./server/sessionStore');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
